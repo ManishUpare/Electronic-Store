10ec61f8210053d03f12d7bd113b4024791e86d1
ManishUpare <mnupare12@gmail.com>
2023-06-19 15:04:55 +0530

    Created ProductDto class

---
+++ src/main/java/com/bikkadIT/ElectronicStore/dtos/ProductDto.java
@@ -0,0 +1,26 @@
+package com.bikkadIT.ElectronicStore.dtos;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import java.util.Date;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ProductDto {
+
+    private String productId;
+
+    private String title;
+
+    private String description;
+    private Integer price;
+    private Integer discountedPrice;
+    private Integer quantity;
+    private Date addedDate;
+    private Boolean live;
+    private Boolean stock;
+}
