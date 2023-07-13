1dde5d3b9519368d2c9e95b03822699933c5d1e4
ManishUpare <mnupare12@gmail.com>
2023-07-13 18:35:51 +0530

    Create getAllCategory Test in CategoryService_Test

--- src/test/java/com/bikkadIT/ElectronicStore/services/CategoryServiceTest.java
+++ src/test/java/com/bikkadIT/ElectronicStore/services/CategoryServiceTest.java
@@ -2,6 +2,8 @@ package com.bikkadIT.ElectronicStore.services;
 
 import com.bikkadIT.ElectronicStore.dtos.CategoryDto;
 import com.bikkadIT.ElectronicStore.entities.Category;
+import com.bikkadIT.ElectronicStore.entities.Product;
+import com.bikkadIT.ElectronicStore.helper.PageableResponse;
 import com.bikkadIT.ElectronicStore.repositories.CategoryRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,7 +13,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,9 +38,9 @@ class CategoryServiceTest {
     Category category;
 
     @BeforeEach
-    public void init(){
+    public void init() {
 
-     category = Category.builder()
+        category = Category.builder()
                 .title("Mobiles")
                 .description("This category contains Mobiles")
                 .coverImage("mobile.png")
@@ -51,14 +58,14 @@ class CategoryServiceTest {
         System.out.println(dto.getTitle());
 
         Assertions.assertNotNull(dto);
-        Assertions.assertEquals("Mobiles",dto.getTitle());
+        Assertions.assertEquals("Mobiles", dto.getTitle());
 
     }
 
     @Test
     void updateCategory_Test() {
 
-        CategoryDto categoryDto=CategoryDto.builder().title("New Mobiles").description("This is categoryDto").coverImage("newMob.png").build();
+        CategoryDto categoryDto = CategoryDto.builder().title("New Mobiles").description("This is categoryDto").coverImage("newMob.png").build();
 
         Mockito.when(categoryRepository.findById(Mockito.anyString())).thenReturn(Optional.of(category));
         Mockito.when(categoryRepository.save(Mockito.any())).thenReturn(category);
@@ -83,17 +90,37 @@ class CategoryServiceTest {
 
         categoryService.deleteCategory(Mockito.anyString());
 
-        Mockito.verify(categoryRepository,Mockito.times(1)).delete(category);
-
-
+        Mockito.verify(categoryRepository, Mockito.times(1)).delete(category);
 
     }
 
     @Test
     void getAllCategory() {
+
+        Category c1 = Category.builder()
+                .title("Washing Machine")
+                .description("This category contains Washing Machine")
+                .coverImage("wash.png")
+                .build();
+
+        Category c2 = Category.builder()
+                .title("TV")
+                .description("This category contains TV")
+                .coverImage("tv.png")
+                .build();
+
+
+        List<Category> list = Arrays.asList(category,c1,c2);
+
+        Page<Category> page = new PageImpl<>(list);
+
+        Mockito.when(categoryRepository.findAll((Pageable) Mockito.any())).thenReturn(page);
+
+        PageableResponse<CategoryDto> allCategory = categoryService.getAllCategory(1, 1, "title", "asc");
+
+        Assertions.assertEquals(3,allCategory.getContent().size());
+
     }
 
-    @Test
-    void getSingleCategory() {
-    }
+
 }
