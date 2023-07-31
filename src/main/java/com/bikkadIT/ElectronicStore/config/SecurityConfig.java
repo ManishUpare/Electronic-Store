17505b5f602866c28fe8a5cee041fe1c88909e46
ManishUpare <mnupare12@gmail.com>
2023-07-31 17:56:43 +0530

    Implemented UserDetails in User Entity

--- src/main/java/com/bikkadIT/ElectronicStore/config/SecurityConfig.java
+++ src/main/java/com/bikkadIT/ElectronicStore/config/SecurityConfig.java
@@ -12,26 +12,26 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService(){
-
-        UserDetails normal = User.builder()
-                .username("MANISH")
-                .password(passwordEncoder().encode("manish"))
-                .roles("NORMAL")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("RUDRA")
-                .password(passwordEncoder().encode("rudra"))
-                .roles("ADMIN")
-                .build();
-
-
-        //InMemoryUserDetailsManager is implemented class of UserDetailsService
-
-        return new InMemoryUserDetailsManager(normal,admin);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//
+//        UserDetails normal = User.builder()
+//                .username("MANISH")
+//                .password(passwordEncoder().encode("manish"))
+//                .roles("NORMAL")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("RUDRA")
+//                .password(passwordEncoder().encode("rudra"))
+//                .roles("ADMIN")
+//                .build();
+//
+//
+//        //InMemoryUserDetailsManager is implemented class of UserDetailsService
+//
+//        return new InMemoryUserDetailsManager(normal,admin);
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
