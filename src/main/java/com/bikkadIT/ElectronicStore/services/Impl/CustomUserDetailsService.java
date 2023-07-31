d471eb0aba76127890e2e57162a6ab23c54ff191
ManishUpare <mnupare12@gmail.com>
2023-07-31 18:08:22 +0530

    Implemented CustomUserDetailsService in ServiceImpl

---
+++ src/main/java/com/bikkadIT/ElectronicStore/services/Impl/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.bikkadIT.ElectronicStore.services.Impl;
+
+import com.bikkadIT.ElectronicStore.entities.User;
+import com.bikkadIT.ElectronicStore.exceptions.ResourceNotFoundException;
+import com.bikkadIT.ElectronicStore.helper.AppConstant;
+import com.bikkadIT.ElectronicStore.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new ResourceNotFoundException(AppConstant.NOT_FOUND));
+
+        return user;
+
+    }
+}
